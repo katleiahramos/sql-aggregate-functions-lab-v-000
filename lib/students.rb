4af8f3ebef94cc3b620315ac9d81a3a7184c3d5f
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-08 16:36:43 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  'SELECT MIN()'
+  'SELECT MIN(GPA) from students;'
 end
 
 def lowest_student_gpa
